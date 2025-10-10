f3f7a5f3998b4f7835d6b21e4189fc2b337af37e
actioncrew <sepeloleksij4@gmail.com>
2025-10-10 09:42:16 +0300

    removed sorting

--- src/html-generator.ts
+++ src/html-generator.ts
@@ -890,7 +890,6 @@ window.HMRClient = (function() {
 
     function listTests() {
       const specs = getAllSpecs();
-      specs.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })); 
       console.table(specs.map(s => ({
         id: s.id,
         name: s.description,
